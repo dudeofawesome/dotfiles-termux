6d398f6bc8c2c4991edb044919c449eca3c9c194
Louis Orleans <louis@orleans.io>
2017-08-06 21:26:49 -0700

    style(installer): removing newlines

--- get.sh
+++ get.sh
@@ -1,14 +1,11 @@
 #!$PREFIX/bin/env bash
 
-
 pkg install git
 mkdir github
 pushd github
 git clone https://github.com/dudeofawesome/dotfiles-termux.git
-
 cd dotfiles-termux 
 
 ./install.sh
 
 popd
-
